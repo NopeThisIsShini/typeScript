0a398fa8304aed5994dd5d073c994cf2f2d71247
NopeThisIsShini <getin2santa@gmail.com>
2024-09-15 11:18:05 +0530

    string datatype

---
+++ datatypes.js
@@ -0,0 +1,7 @@
+"use strict";
+// string datatype
+// let lname = 'John';
+// typeScript has the ability to determine the type of the variable by it's value 
+// console.log(lname);
+let fname = 'santa';
+console.log(fname);
---
+++ datatypes.ts
@@ -0,0 +1,6 @@
+// string datatype
+let lname = 'John';
+// typeScript has the ability to determine the type of the variable by it's value 
+console.log(lname);
+let fname: string = 'santa'
+console.log(fname)
